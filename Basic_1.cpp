efa929d110aa2918b1a9b93bd7bb7c1a9861be07
Sameer Kumar <sameer2001kr@gmail.com>
2023-01-22 15:56:04 +0530

    first commit

---
+++ Basic_1.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int check(int number, int array[], int size){
+    int count = 0;
+
+    for(int i=0; i<size; i++){
+        if(number == array[i])
+            count++;
+    }
+    return count;
+    
+}
+
+int main()
+{
+    int size;
+    cin >> size;
+
+    int arr[size];
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int q;
+    cin >> q;
+    while(q--){
+        int num; 
+        cin >> num;
+        cout << check(num, arr, size);
+    }
+    return 0;
+}
